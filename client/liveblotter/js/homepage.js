c2c20a8977cadf9a99d993e437b50c2259167534
Daniel Fang <danfang@uw.edu>
2015-01-24 02:49:15 +0000

    Client formatting

--- client/liveblotter/js/homepage.js
+++ client/liveblotter/js/homepage.js
@@ -4,8 +4,21 @@
 var url = "ws://code.danielfang.org/liveblotter/ws/";
 var ws = new WebSocket(url);
 var crimes = [];
+var now = new Date();
 
 $(document).ready(function() {
+
+    var titleHeight = $("#title-bar").height();
+
+    resizeContent($(window).height() - titleHeight);
+
+    $(window).resize(function() {
+        resizeContent($(window).height() - titleHeight);
+    });
+
+    setInterval(function() {
+        now = new Date();
+    }, 1000);
 
     // Google Maps init
 	initialize();
@@ -23,7 +36,20 @@ $(document).ready(function() {
             for (i in data.existing) {
                 var crime = data.existing[i];
                 crimes.push(crime);
-                html += "<p>" + crime.event_clearance_date + ':  ' + crime.event_clearance_description + "</p>";
+
+                var date = new Date(crime.event_clearance_date);
+                date.setHours(date.getHours() + 8);
+                var ago = (now - date) / (1000 * 60);
+                var agoString;
+                if (ago > 60) {
+                    agoString = "<div class=\"date\">" + parseInt(ago / 60) + " hours ago" + 
+                        "</div>";
+                } else {
+                    agoString = parseInt(ago) + " minutes ago";
+                }
+
+                html += "<div>" + agoString + "<p>" + 
+                    crime.event_clearance_description + "</p></div>";
             }
             $("#feed").html(html);
         } else if (data.hasOwnProperty("new")) {
@@ -33,22 +59,28 @@ $(document).ready(function() {
             for (i in data.new) {
                 var crime = data.new[i];
                 crimes.splice(i, 0, crime);
-                html += "<p>" + crime.event_clearance_date + ':  ' + 
+
+                var date = Date.parse(crime.event_clearance_date);
+                var ago = now - date;
+
+                html += "<p>" + ago + 'ago:  ' + 
                     crime.event_clearance_description + "</p>";
+                $("#feed").last().remove();
             }
 
             $("#feed").html(html + $("#feed").html()); 
         }
     };
-
-
 });
 
+function resizeContent(height) {
+    $("#content-container").css("height", height);
+}
 
 function initialize() {
 	var mapOptions = {
 		center: { lat: 47.6063889, lng: -122.3308333},
-		zoom: 8
+		zoom: 12
 	};
 	var map = new google.maps.Map(document.getElementById('map'),
 		mapOptions);
